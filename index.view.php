6412a516271efe182f7dfc18508e68bd1570ca99
Brad Mallett <bmallett2@gmail.com>
2018-03-20 19:55:07 +0000

    adding mysql

---
+++ index.view.php
@@ -0,0 +1,20 @@
+
+<html>
+<head>
+    <title>Quotes</title>
+    <link href="main.css" type="text/css" rel="stylesheet"/>
+   
+</head>
+<body style="background-image: url(http://photos.vanityfair.com/2015/09/23/5602b884873a015e454b1e52_yogi-berra-new-york-yankees-remembrance-obituary.jpg
+); background-repeat: no-repeat; width: 100%; height: 100%; background-position: center; background-size: cover;">
+
+    <div class="quote">
+        <p> "Always go to other people’s funerals, otherwise they won’t come to yours."</p>
+        <p>-Yogi Berra</p>
+    </div><!--quote-->
+    
+</body>
+</html>
+
+<html>
+    
